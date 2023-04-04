1666571569e03f5726b28926b5f1af19aa13d0d3
Paiigio <duongntph29011@fpt.edu.vn>
2023-04-04 10:05:04 +0700

    no message

--- src/view/Login.java
+++ src/view/Login.java
@@ -6,15 +6,7 @@ import ViewModel.NhanVienModel;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import Mail.ResetPass;
-import java.util.Properties;
 import java.util.Random;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 
 public class Login extends javax.swing.JFrame {
 
